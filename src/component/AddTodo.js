4189b9f17645ad0e975b386df7722365147d2069
kenlos <kenlosxd@gmail.com>
2022-06-19 20:41:12 -0500

    Functional todo list

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -7,6 +7,7 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
+      date: "",
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user.
@@ -15,18 +16,21 @@ class AddTodo extends Component {
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
+      date: Date().toLocaleString('en-US'), //not sure if correct
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
   // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
   // this.props.addTodo(this.state) passes the current state (or user input) into the addTodo function defined
   // in the Home.js file which then adds the input into the list.
-  handleSubmit = (event) => {
+  handleSubmit = (event, date) => {
     event.preventDefault();
+    
     if (this.state.content.trim()) {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
+        date: "", //edited
       });
     }
   };
